d0bbd78ecbfbf400e1068c094d48160ed05fabc8
neha-kapoor <neha.kapoor@tenthmatrix.co.uk>
2017-07-18 13:48:29 +0530

    Changed the constants file path

--- public_html/include/connect.php
+++ public_html/include/connect.php
@@ -4,7 +4,7 @@ include_once "ez_sql_core.php";
 // Include ezSQL database specific component
 include_once "ez_sql_mysql.php";
 
-include_once "../private_config/constants.php";
+include_once "../../config/C7CA2AA6-9AF1-7B3D-7857-42D93BC4E97A.php";
 
 $db = new ezSQL_mysql(DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);
 ?>
